8604f3816660c60e730328e981eb5a79c5db5446
ahmadmarhaba <dragon.8881@hotmail.com>
2022-08-15 01:01:57 +0300

    edit fix url

--- store/actions/socketAction.tsx
+++ store/actions/socketAction.tsx
@@ -1,6 +1,6 @@
 import * as types from '../types'
 import { io } from 'socket.io-client'
-const ENDPOINT = "https:ahmadmarhaba-chat-app-nextjs.herokuapp.com"; //"http://localhost:5000"
+const ENDPOINT = "https://ahmadmarhaba-chat-app-nextjs.herokuapp.com"; //"http://localhost:5000"
 
 export const fetchSocket = () => async (dispatch : any) => {
     dispatch({
