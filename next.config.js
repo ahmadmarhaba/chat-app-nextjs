e8f37276fc02bafbb5ee95b8fa38ac1fc076b5b8
ahmadmarhaba <dragon.8881@hotmail.com>
2022-08-14 21:13:19 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@
 module.exports = {
   reactStrictMode: false,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 }
