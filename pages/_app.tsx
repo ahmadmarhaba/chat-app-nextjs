2aad228470d3f63635c9a54f22e089d823ad4d00
ahmadmarhaba <dragon.8881@hotmail.com>
2022-08-25 22:09:35 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import store from '../store/store';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: 'AIzaSyADhLUAb4Guot2ev1md_yE-rPqZnin5qi0',
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
